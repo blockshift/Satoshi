b581cdda0caeba9fa02df739b8e72a5fca63c9a3
mohammadobaid1 <mobaid56@gmail.com>
2018-01-08 12:28:31 +0500

    Login

--- Angular4_CLI_Full_Project/src/app/views/components/forms.component.ts
+++ Angular4_CLI_Full_Project/src/app/views/components/forms.component.ts
@@ -31,7 +31,7 @@ isCopied1: boolean = false;
   	.subscribe(data => {
   	           console.log('Service',form.name); 
                     var testRespons = data.token;                     
-                    this.dataContainer.nativeElement.innerHTML = "";
+                    this.dataContainer.nativeElement.innerHTML = data.token;
        
                      console.log("I SEE DATA HERE: ",testRespons);
                
